16d8950093abe6216015d61e5e08e46ae9aa116d
Benoît Mériaux <benoit.meriaux@datadoghq.com>
2023-07-05 17:05:55 +0200

    solutions until quizz 2

--- exercises/intro/intro1.rs
+++ exercises/intro/intro1.rs
@@ -9,7 +9,6 @@
 // when you change one of the lines below! Try adding a `println!` line, or try changing
 // what it outputs in your terminal. Try removing a semicolon and see what happens!
 
-// I AM NOT DONE
 
 fn main() {
     println!("Hello and");
@@ -29,7 +28,6 @@ fn main() {
     println!(
         "Going forward, the source of the exercises will always be in the success/failure output."
     );
-    println!();
     println!(
         "If you want to use rust-analyzer, Rust's LSP implementation, make sure your editor is set"
     );
